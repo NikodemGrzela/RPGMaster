7cf3c1325f2784d903dd8c8c70b79fc57aa1dbfe
NikodemGrzela <106999662+NikodemGrzela@users.noreply.github.com>
2025-11-04 13:36:06 +0100

    Create CharacterSheetScreen widget for character display

---
+++ rpgmaster/lib/screens/characterSheet/character_sheet.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class CharacterSheetScreen extends StatelessWidget {
+  final String characterName;
+  final String campaignName;
+
+  const CharacterSheetScreen({
+    super.key,
+    required this.characterName,
+    required this.campaignName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text(characterName),
+        actions: [
+          IconButton(
+            onPressed: () {
+              //TODO: zmiana postaci
+            },
+            icon: const Icon(Icons.swap_horiz),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Scrollowalna sekcja - zajmuje dostępne miejsce
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: List.generate(
+                  20, // Przykładowa liczba widgetów
+                      (index) => Container(
+                    margin: EdgeInsets.only(bottom: 12.0),
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(color: Colors.grey.shade300),
+                    ),
+                    child: Text(
+                      'Widget numer ${index + 1}',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // Divider oddzielający
+          Divider(
+            height: 1.0,
+            thickness: 1.0,
+            color: Colors.grey.shade400,
+          ),
+
+          // Sekcja z przyciskami
+          Container(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+
+                Expanded(
+                  child: FilledButton(
+                    onPressed: () {
+                      // TODO: Nawigacja do notatek
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.notes),
+                        SizedBox(width: 8.0),
+                        Text('Notatki'),
+                      ],
+                    ),
+                  ),
+                ),
+
+                SizedBox(width: 16.0),
+
+                Expanded(
+                  child: FilledButton(
+                    onPressed: () {
+                      // TODO: Nawigacja do rzutu kością
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.casino),
+                        SizedBox(width: 8.0),
+                        Text('Rzuć kością'),
+                      ],
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
