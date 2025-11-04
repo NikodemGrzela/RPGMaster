386b5b9524ac358d1f6d772f9bd70d78cba24679
Ellemire <julia.ellemire@gmail.com>
2025-11-04 23:09:17 +0100

    Updated text styles

--- rpgmaster/lib/ui/theme/app_text_styles.dart
+++ rpgmaster/lib/ui/theme/app_text_styles.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 
 class AppTextStyles {
-  static const TextStyle headline = TextStyle(
-    fontSize: 24,
+
+  /// Logo font
+  static const TextStyle logo = TextStyle(
+    fontFamily: 'UncialAntiqua',
+    fontWeight: FontWeight.w300,
+    fontSize: 64,
+  );
+
+  /// Big headlines
+  static const TextStyle title = TextStyle(
+    fontSize: 32,
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle body = TextStyle(
-    fontSize: 16,
+  /// Regular headlines
+  static const TextStyle headline = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
   );
 
+  /// Regular text
+  static const TextStyle body = TextStyle(
+    fontSize: 18,
+  );
+
+  /// Small text
   static const TextStyle label = TextStyle(
-    fontSize: 12,
+    fontSize: 14,
     letterSpacing: 0.5,
   );
 }
